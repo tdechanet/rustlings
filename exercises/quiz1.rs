b318dfd4f9efc318aa2906ec10b842116b8b3837
tdechanet <titouan.dechanet@outlook.com>
2024-06-28 15:37:09 +0200

    resolved first quizz

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
-// Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(nmb_of_apples:u16) -> u16 {
+    if nmb_of_apples > 40 {
+        nmb_of_apples
+    }
+    else {
+        nmb_of_apples * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
